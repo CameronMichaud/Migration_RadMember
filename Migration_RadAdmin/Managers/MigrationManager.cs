2b1093b332c46e1c8c571a74bf72c33a02f470e9
Cameron Michaud <cmich2001@gmail.com>
2025-08-02 20:48:43 -0400

    Updated to migrate member-side instead of admin

--- Migration_RadAdmin/Managers/MigrationManager.cs
+++ Migration_RadAdmin/Managers/MigrationManager.cs
@@ -14,56 +14,6 @@ namespace Migration_RadAdmin.Migration
             form = mainForm;
         }
 
-        internal static async Task InstallDotNets()
-        {
-            string currentUser = Environment.UserName;
-
-            await form.RunTask("Installing .NET SDKs", form.dotnetProgress, async () =>
-            {
-                bool dotnet6 = InstallManager.DotNetInstalled("6");
-                if (!dotnet6)
-                {
-                    // DOWNLOAD
-                    OutputManager.Log("Dowloading .NET 6 (this may take a few minutes)");
-                    string URL = "https://builds.dotnet.microsoft.com/dotnet/Sdk/6.0.428/dotnet-sdk-6.0.428-win-x64.exe";
-                    await InstallManager.GetInstaller(URL, $@"C:\\users\{currentUser}\downloads\dotnet6.exe");
-                    InstallDotNet(currentUser, "6");
-                }
-                else
-                {
-                    OutputManager.Log(".NET 6 already installed");
-                }
-
-                OutputManager.setProgress(form.dotnetProgress, 50);
-
-                bool dotnet8 = InstallManager.DotNetInstalled("8");
-                if (!dotnet8)
-                {
-                    // DOWNLOAD
-                    OutputManager.Log("Dowloading .NET 8 (this may take a few minutes)");
-                    string URL = "https://builds.dotnet.microsoft.com/dotnet/Sdk/8.0.100/dotnet-sdk-8.0.100-win-x64.exe";
-                    await InstallManager.GetInstaller(URL, $@"C:\\users\{currentUser}\downloads\dotnet8.exe");
-                    InstallDotNet(currentUser, "8");
-                }
-                else
-                {
-                    OutputManager.Log(".NET 8 already installed");
-                }
-            });
-        }
-
-        internal static void InstallDotNet(string currentUser, string version)
-        {
-            OutputManager.setProgress(form.dotnetProgress, 75);
-
-            // INSTALL
-            OutputManager.Log($"Installing .NET {version} (this may take a few minutes)");
-            ProcessManager.RunTerminal("cmd.exe", $@"/c C:\\users\{currentUser}\downloads\dotnet{version}.exe /quiet");
-            OutputManager.Log($".NET {version} install finished.\n");
-
-            File.Delete($@"C:\\users\{currentUser}\downloads\dotnet{version}.exe");
-        }
-
         internal static async Task UpdateUsers()
         {
             string currentUser = Environment.UserName;
@@ -101,7 +51,7 @@ namespace Migration_RadAdmin.Migration
                 {
                     // If there's no package manager, download the file
                     string chromeInstaller = @"https://dl.google.com/chrome/install/latest/chrome_installer.exe";
-                    string filePath = $@"C:\\users\{currentUser}\downloads\chrome_installer.exe";
+                    string filePath = $@"C:\users\{currentUser}\downloads\chrome_installer.exe";
                     await InstallManager.GetInstaller(chromeInstaller, filePath);
 
                     OutputManager.setProgress(form.chromeProgress, 75);
@@ -118,29 +68,12 @@ namespace Migration_RadAdmin.Migration
             });
         }
 
-        internal static async Task InstallSkyview()
-        {
-            await form.RunTask("Installing Skyview Services", form.cleanProgress, async () =>
-            {
-                OutputManager.setProgress(form.cleanProgress, 50);
-
-                // Install Skyview services
-                InstallManager.GetServices();
-            });
-        }
-
         internal static void CompleteMigration()
         {
             OutputManager.setStatus("Migration Complete!");
             form.startButton.Text = "Complete";
 
             MessageBox.Show("Migration completed successfully.", "Migration Complete!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        internal static async Task DeleteServiceCentral()
-        {
-            OutputManager.setStatus(@"Deleting Service Central (C:\ProgramData\Radianse)");
-            await Task.Run(() => ProcessManager.DeleteServiceCentral());
         }
 
         internal static void SetStartup()
